ef1fa66e2c59a57c25281f495d3e4e6221bad0bf
Nur Rachmat <khalbhidiary@gmail.com>
2022-05-13 09:48:57 +0700

    CRUD

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -10,22 +10,10 @@ class ProdiController extends Controller
 {
 
     function index(){
-        /*$data = [
-            'prodi' => ['Manajemen Informatika', 'Sistem Informasi', 'Informatika']
-        ];
     
-        //atau menggunakan compact
-        $prodi = ['Manajemen Informatika', 'Sistem Informasi', 'Informatika'];
-        $kampus = "Universitas Multi Data Palembang";
-    
-        return view("prodi.index", compact('prodi', 'kampus'));*/
-
         $kampus = "Universitas Multi Data Palembang";
         $prodi = Prodi::all();
-
-        /*$prodi = DB::select("SELECT prodi.*, fakultas.nama as namaf FROM prodi INNER JOIN fakultas ON prodi.fakultas_id = fakultas.id");*/
-        
-        return view("prodi.index", compact('kampus', 'prodi'));
+        return view('prodi.index')->with('prodi', $prodi);
     }
 
     function detail($id = null){
@@ -34,5 +22,50 @@ class ProdiController extends Controller
 
     function create(){
         return view("prodi.create");
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+       
+        $prodi = new Prodi(); //buat object prodi
+        $prodi->nama = $validateData['nama']; //simpan nilai inout ($validateData['nama]) ke dalam property nama prodi ($prodi->nama)
+        $prodi->institusi_id = 0;
+        $prodi->fakultas_id = 1;
+        $prodi->save(); //simpan ke dalam tabel prodis
+
+        //return "Data prodi $prodi->nama berhasil disimpan ke database"; // tampilkan pesan berhasil
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil disimpan ke database");
+        return redirect()->route('prodi.create');
+    }
+
+    public function show(Prodi $prodi)
+    {
+        return view('prodi.show', ['prodi' => $prodi]);
+    }
+
+    public function edit(Prodi $prodi)
+    {
+        return view('prodi.edit', ['prodi' => $prodi]);
+    }
+
+    public function update(Request $request, Prodi $prodi)
+    {
+     
+        $validateData = $request->validate([
+            'nama' => 'required|min:5|max:20',
+        ]);
+
+        Prodi::where('id', $prodi->id)->update($validateData);
+        $request->session()->flash('info', "Data prodi $prodi->nama berhasil diubah");
+        return redirect()->route('prodi.index');
+    }
+
+    public function destroy(Prodi $prodi)
+    {
+        $prodi->delete();
+        return redirect()->route('prodi.index')->with("info", "Prodi $prodi->nama berhasil dihapus.");
     }
 }
